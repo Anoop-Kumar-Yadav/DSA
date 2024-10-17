800757f122c546c9f141db4e4700a97db20a7920
Anoop Kumar Yadav <anoop9569110314@gmail.com>
2024-10-17 23:27:04 +0530

    Array LeetCode Session Complete Now Moving Towards 2-D Array

---
+++ _03_Array/Ex_06_LC42.cpp
@@ -0,0 +1,81 @@
+/*
+  
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void display(int v[] ,int size){
+    for (int i = 0; i < size; i++) {
+        cout<<v[i]<<" ";
+    }  
+    cout<<endl;  
+}
+
+int trap(vector<int>& height) {
+
+    int water_ = 0;
+    int size = height.size();
+
+    // previous greates element array
+    int pre_greatest[size];
+    pre_greatest[0] = -1;
+    int max_ = height[0];
+    for (int i = 1; i < size; i++) {
+        pre_greatest[i] = max_;
+        if ( max_ < height[i]) max_ = height[i];
+    }
+
+    // next greatest element array    
+    pre_greatest[size-1] = -1;
+    max_ = height[size-1];
+    for (int i = size-2; i >= 0; i--) {
+        if(max_ < pre_greatest[i]) pre_greatest[i] = max_;
+        if (max_ < height[i]) max_ = height[i] ;
+    }   
+    
+    // // minimum array 
+    // for (int i = 0; i < size; i++) {
+    //     pre_greatest[i] = (pre_greatest[i]>next_greatest[i])? next_greatest[i] : pre_greatest[i];
+    // }
+
+    for (int i = 1; i < size-1; i++) {
+        if (height[i] < pre_greatest[i]){
+            water_ = water_ + (pre_greatest[i]-height[i]);
+        }
+    }
+    return water_ ;
+}
+
+int main() {
+    vector<int> v;
+    // v.push_back(0);
+    // v.push_back(1);
+    // v.push_back(0);
+    // v.push_back(2);
+    // v.push_back(1);
+    // v.push_back(0);
+    // v.push_back(1);
+    // v.push_back(3);
+    // v.push_back(2);
+    // v.push_back(1);
+    // v.push_back(2);
+    // v.push_back(1);
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(0);
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(5);
+
+    int filled_water = trap(v);
+    cout<<"Filled : "<<filled_water;
+
+    return 0;
+}
+
+/*
+Author: Your Name
+Date: Date
+*/
